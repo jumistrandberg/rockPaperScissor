9200d378ebd9a2b56d5b8838d1ffec02b56ee423
Jumi <feliciastrandberg@gmail.com>
2023-11-15 11:19:46 +0100

    created computer pick function with random number

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Global variables 
 const rockBtn = document.getElementById('rockBtn');
 const paperBtn = document.getElementById('paperBtn');
 const scissorsBtn = document.getElementById('scissorsBtn');
@@ -5,14 +6,31 @@ const scissorsBtn = document.getElementById('scissorsBtn');
 rockBtn.addEventListener('click', function(event) {
     event.preventDefault();
     console.log('rock')
-})
+    computerPick();
+});
 
 paperBtn.addEventListener('click', function(event) {
     event.preventDefault();
     console.log('paper')
-})
+    computerPick()
+});
 
 scissorsBtn.addEventListener('click', function(event) {
     event.preventDefault();
     console.log('scissors')
-})
+    computerPick()
+});
+
+// Computer pick 
+
+function computerPick() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if(randomNumber === 1) {
+        console.log('computer rock')
+    } else if (randomNumber === 2) {
+        console.log('computer paper')
+    } else {
+        console.log('computer scissors')
+    }
+}
