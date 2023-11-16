506c1c7253f9d58dd53f3e84c6ad31e76980dd20
Jumi <feliciastrandberg@gmail.com>
2023-11-16 09:47:18 +0100

    created score keeping array for win-loss logic. Working as expected

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const rockBtn = document.getElementById("rockBtn");
 const paperBtn = document.getElementById("paperBtn");
 const scissorsBtn = document.getElementById("scissorsBtn");
 const winLoss = document.getElementById("winLoss");
+const scoreBoard = document.getElementById('scoreBoard');
 let randomNumber;
 let userPick;
+let winLossCount = [];
+let winCount;
+let lossCount;
 
 rockBtn.addEventListener("click", function (event) {
   event.preventDefault();
@@ -51,14 +55,31 @@ function computerPick() {
 function comparePicks() {
   if (userPick === randomNumber) {
     winLoss.innerText = "Draw";
+    winLossCount.push('draw');
+    scoreBoard.innerText = winLossCount.join(', ');
+
   } else if (
     (userPick === 1 && randomNumber === 3) ||
     (userPick === 2 && randomNumber === 1) ||
     (userPick === 3 && randomNumber === 2))
   {
-      winLoss.innerText = "You win"
+      winLoss.innerText = "You win";
+      winLossCount.push('win');
+      scoreBoard.innerText = winLossCount.join(', ');
+      winCount++;
+
     } else {
-      winLoss.innerHTML = "Computer wins"
+      winLoss.innerHTML = "Computer wins";
+      winLoss.innerText = "You lose";
+      winLossCount.push('lose');
+      scoreBoard.innerText = winLossCount.join(', ');
+      lossCount++;
     }
-    
+    scoreCounter();
+}
+
+function scoreCounter() {
+  if(winCount === 5) {
+   
+  }
 }
